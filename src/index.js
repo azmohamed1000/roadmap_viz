efb54f3e71f6872e702b05c4e2b39889bd5347ae
azmohamed1000 <azizimohamed7854@gmail.com>
2025-04-30 16:14:23 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import dscc from '@google/dscc';
-import RoadmapViz from './roadmapViz';
-
-dscc.subscribe((data, env) => {
-  const viz = new RoadmapViz('#container', data.tables.DEFAULT, env);
-  viz.render();
-});
